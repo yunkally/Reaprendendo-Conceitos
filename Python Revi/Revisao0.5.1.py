42ea6fe8be48ae4d6fe3dc628e8ecd7673f71205
yunkally <taldospike131@gmail.com>
2026-05-29 19:10:41 -0300

    Aula 5: Conceitos de Dicionários - Criando e adicionando chaves e valores

---
+++ Python Revi/Revisao0.5.1.py
@@ -0,0 +1,11 @@
+#Dicionário
+
+perfil = {
+    'nome': 'Allyson',
+    'idade': 18,
+    'jogo favorito': 'Valorant'
+}
+print(perfil['jogo favorito'])
+perfil['Categoria'] = 'Developer'
+print(perfil['Categoria'])
+print(perfil)
