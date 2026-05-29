05033b872325f911330dd8f04ccb161906cc850b
yunkally <taldospike131@gmail.com>
2026-05-28 22:08:12 -0300

    Aula 4: Criando, adicionando itens e percorrendo listas em Python

---
+++ Python Revi/revisão0.4.1.py
@@ -0,0 +1,8 @@
+#Trabalhando com listas
+
+inventario = ['Picareta de diamante', 'Espada de ferro', 'Tocha',]
+print(inventario[0])
+inventario.append('Bloco de diamante')
+print(inventario)
+for item in inventario:
+    print(item)
