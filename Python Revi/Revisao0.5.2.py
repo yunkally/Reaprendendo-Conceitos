3aa79918042d3173b4517bc3d2f110a713b1802c
yunkally <taldospike131@gmail.com>
2026-06-01 18:53:01 -0300

    Revisão Geral: Códigos limpos e gabaritados das Aulas 4 e 5

--- Python Revi/Revisao0.5.2.py
+++
@@ -1,9 +0,0 @@
-agente = {
-    'nome': 'Jett',
-    'funcao': 'Duelista',
-    'pais': 'Coreia do Sul'
-}
-agente['habilidade'] = 'Tormenta de aço'
-print(agente['habilidade'])
-for chave, valor in agente.items():
-    print(f'{chave}: {valor}')
--- Python Revi/revisão0.4.1.py
+++ Python Revi/revisão0.4.1.py
@@ -5,4 +5,4 @@ print(inventario[0])
 inventario.append('Bloco de diamante')
 print(inventario)
 for item in inventario:
-    print(item)
+    print(item) 
